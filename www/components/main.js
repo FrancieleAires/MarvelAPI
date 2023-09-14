8300cb781fb96c3b43b46b6db89d0efd5712d2f0
FrancieleAires <f.aires2016@gmail.com>
2023-09-15 02:56:42 +0900

    Finalmente deu certo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- www/components/main.js
+++ www/components/main.js
@@ -1,29 +1,60 @@
-        document.addEventListener('DOMContentLoaded', ()=> {
+let ts = "1694646496";
+let publicKey = "0199f3cfc44b942137bc8dc6b45938d2"
+let hashValue = "3161e07161b2bbe6765221701224aa0d"
+const divListaSeries = document.getElementById('listaSeries');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const clearButton = document.getElementById('clear-button');
+const imagens2 = document.querySelector('.imagens2');
 
-            const myHash = '7d8dab1e53981c3edd4fbbee4378c728';
-            const myPublicKey = '0199f3cfc44b942137bc8dc6b45938d2';
-            const myTimestamp = '1692300644'; 
-            const myEndPoint = 'comics';   
-            const myEndPoint1 = 'characters';       
-            const divListaSeries = document.getElementById('listaSeries');
+const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
 
-            fetch(`http://gateway.marvel.com/v1/public/comics?ts=${myTimestamp}&apikey=${myPublicKey}&hash=${myHash}`)
-            .then((response)=> {
-                return response.json();
-            }).then((result)=> {
-                const listaDeResultados = result.data.results;
 
-                console.log(listaDeResultados)
+function removeElements() {
+  listContainer.innerHTML = "";
+}
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
 
-                listaDeResultados.forEach(item => {
-                   divListaSeries.insertAdjacentHTML('beforeend',
-                   `<div>
-                        <h6>${item.title}</h6>
-                        <img src="${item.thumbnail.path}.${item.thumbnail.extension}">
-                        </div>`);
-                    
-                });
-            })
-      })
-      
+  const response = await fetch(url);
+  const jsonData = await response.json();
+
+  jsonData.data["results"].forEach((result) => {
+    let name = result.name;
+    let div = document.createElement("div");
+    div.style.cursor = "pointer";
+    div.classList.add("autocomplete-items");
+    div.setAttribute("onclick", "displayWords('" + name + "')");
+    let word = "<b>" + name.substr(0, input.value.length) + "</b>";
+    word += name.substr(input.value.length);
+    div.innerHTML = `<p class="item">${word}</p>`;
+    listContainer.appendChild(div);
+  });
+
+
+button.addEventListener(
+  "click",
+  (getRsult = async () => {
+    if (input.value.trim().length < 1) {
+      alert("Input cannot be blank");
+    }
+    showContainer.innerHTML = "";
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
+
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    jsonData.data["results"].forEach((element) => {
+      showContainer.innerHTML = `<div class="card-container">
+        <div class="container-character-image">
+        <img src="${
+          element.thumbnail["path"] + "." + element.thumbnail["extension"]
+        }"/></div>
+        <div class="character-name">${element.name}</div>
+        <div class="character-description">${element.description}</div>
+        </div>`;
+    });
+  })
+);
+window.onload = () => {
+  getRsult();
+};
 
